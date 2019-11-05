c09ddb1277445a587e41c68925a536507ab1e45c
Reuven <retzion@gmail.com>
2019-11-05 10:50:25 -0700

    Add BugCatcher CI static analysis testing

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,20 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+import os
+
+bugcatcher_ci = __import__('bugcatcher-ci')
+ftl_sid = os.environ.get('FTL_SID') # Keep it secure!
+ftl_project = os.environ.get('FTL_PROJECT') or 'BUGCATCHER CI'
+ftl = bugcatcher_ci.CI(ftl_sid, ftl_project)
+
+def test_with_bugcatcher():
+    print("\nTesting codebase with BugCatcher API...")
+    uploaded = ftl.push(ftl_project, ".")
+    assert uploaded
+    tested = ftl.test(ftl_project, 'medium')
+    assert tested
+
 
 def test_success():
     assert True
